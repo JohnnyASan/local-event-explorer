f5d374917123c133a184ec9af3c623ba7dc8501b
Johnny <johnanthonysanabria@gmail.com>
2025-04-08 14:30:41 -0600

    updated and added error

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        details: resolve(__dirname, "src/event-details/index.html"),
+        // details: resolve(__dirname, "src/event-details/index.html"),
         saved: resolve(__dirname, "src/saved-events/index.html"),
         error: resolve(__dirname, "src/error/index.html"),
       },
